952659deae7cbd701f67db8d73fc30479a870c0a
Eduardo <eduardoc313@gmail.com>
2021-04-06 20:05:25 -0300

    Adicionar arquivos de projeto.

---
+++ CrudProjeto/Controllers/ClienteController.cs
@@ -0,0 +1,94 @@
+﻿using CrudProjeto.Data;
+using CrudProjeto.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudProjeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        //Injeção de dependência
+        private readonly ApplicationDbContext _db;
+
+        public ClienteController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/Cliente
+        [HttpGet]
+       public IEnumerable<Cliente> Get()
+        {
+            return _db.Clientes;
+        }
+
+
+        // GET: api/Cliente/5
+        [HttpGet("{id}")]
+        public ActionResult<Cliente> Get(int id)
+        {
+
+            var c = _db.Clientes.Find(id);
+
+            if(c == null)
+            {
+                return NotFound();
+            }
+
+            return c;
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult<Cliente> Put(int id, Cliente cliente)
+        {
+
+            if (id != cliente.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Update(cliente);
+            _db.SaveChanges();
+
+            return NoContent();
+
+
+        }
+
+
+        // POST: api/Cliente
+        [HttpPost]
+        public ActionResult<Cliente> Post(Cliente cliente)
+        {
+            _db.Clientes.Add(cliente);
+            _db.SaveChanges();
+            return CreatedAtAction("Get", new { id = cliente.Id }, cliente);
+        }
+
+
+        // DELETE: api/Cliente/5
+        [HttpDelete("{id}")]
+        public ActionResult<Cliente> Delete(int id)
+        {
+            var cliente = _db.Clientes.Find(id);
+            if(cliente == null)
+            {
+                return NotFound();
+            }
+
+            _db.Clientes.Remove(cliente);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+
+
+    }
+}
---
+++ CrudProjeto/Migrations/20210406194227_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudProjeto.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
